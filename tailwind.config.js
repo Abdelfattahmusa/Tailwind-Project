07ebb936b2e2c12639836a4bf6ec06fe8d99ae28
Abdelfattah <106418750+Abdelfattahmusa@users.noreply.github.com>
2022-10-08 15:34:08 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: ['./dist/**/*.html'],
+  theme: {
+   extend: {
+      backgroundImage: {
+        'hero-pattern': "url('/banner-bg.png')",
+      }
+    }
+  },
+  variants: {},
+  plugins: [],
+}
